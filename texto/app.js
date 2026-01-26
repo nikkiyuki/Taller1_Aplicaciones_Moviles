1565dadf06d2cb187ba7a2c123e23e1ae8f198d0
chariiicarrillo <zharick.carrillo@upb.edu.co>
2026-01-26 18:23:41 -0500

    cantidad de caracteres ingresados

---
+++ texto/app.js
@@ -0,0 +1,6 @@
+const texto = document.getElementById("texto");
+const contador = document.getElementById("contador");
+
+texto.addEventListener("input", function () {
+  contador.textContent = texto.value.length;
+});
